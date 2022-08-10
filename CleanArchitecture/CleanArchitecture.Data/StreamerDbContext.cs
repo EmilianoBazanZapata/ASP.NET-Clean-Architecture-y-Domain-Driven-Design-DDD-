b7205763371336661c96d4309dba9486515cb1a4
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-08-09 22:15:04 -0300

    Cadena de Conexion

---
+++ CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder) 
+        {
+            optionBuilder.UseSqlServer("Data Source=DESKTOP-UHFA848\\SQLEXPRESS;Initial Catalog=StreamerDB;Integrated Security=True");
+        }
+
+
+
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
