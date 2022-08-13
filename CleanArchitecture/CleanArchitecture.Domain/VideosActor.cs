7896abba0abf86831d7fcc005d7c85f38a7f5a38
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-08-13 19:15:23 -0300

    Modelos con Domain Driven Design

--- CleanArchitecture/CleanArchitecture.Domain/VideosActor.cs
+++ CleanArchitecture/CleanArchitecture.Domain/VideosActor.cs
@@ -1,6 +1,8 @@
-﻿namespace CleanArchitecture.Domain
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain
 {
-    public class VideosActor
+    public class VideosActor : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
