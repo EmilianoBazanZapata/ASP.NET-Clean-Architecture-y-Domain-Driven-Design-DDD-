fdcaa79367b02d06e5d13527ab102a9072cc012d
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-08-13 15:01:29 -0300

    Relación uno a muchos

--- CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -6,15 +6,25 @@ namespace CleanArchitecture.Data
 {
     public class StreamerDbContext : DbContext
     {
-        protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder) 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
         {
             optionBuilder.UseSqlServer("Data Source=DESKTOP-UHFA848\\SQLEXPRESS;Initial Catalog=StreamerDB;Integrated Security=True")
-                         .LogTo(Console.WriteLine, 
-                                        new[] { DbLoggerCategory.Database.Command.Name }, 
+                         .LogTo(Console.WriteLine,
+                                        new[] { DbLoggerCategory.Database.Command.Name },
                                         LogLevel.Information)
                          .EnableSensitiveDataLogging();
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //relacion uno a muchos
+            modelBuilder.Entity<Streamer>()
+                        .HasMany(s => s.Videos)
+                        .WithOne(s => s.Streamer)
+                        .HasForeignKey(s => s.StreamerId)
+                        .IsRequired()
+                        .OnDelete(DeleteBehavior.Restrict);
+        }
 
 
         public DbSet<Streamer>? Streamers { get; set; }
