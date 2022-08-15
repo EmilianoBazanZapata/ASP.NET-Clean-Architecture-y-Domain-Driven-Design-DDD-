dd5491b53928c9bba63648a4c1cd870d811d8a19
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-08-14 23:06:00 -0300

    Excepciones en Validacion

---
+++ CleanArchitecture/CleanArchitecture.Applicattion/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+
+namespace CleanArchitecture.Applicattion.Exceptions
+{
+    public class ValidationExceptions : ApplicationException
+    {
+        public IDictionary<string, string[]> Errors { get; set; }
+
+        public ValidationExceptions() : base("Se presentaron uno o mas errores de validacion")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationExceptions(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures.GroupBy(e => e.PropertyName, 
+                                      e => e.ErrorMessage).ToDictionary(failureGroup => failureGroup.Key, 
+                                                                        failureGroup => failureGroup.ToArray());
+        }
+    }
+}
