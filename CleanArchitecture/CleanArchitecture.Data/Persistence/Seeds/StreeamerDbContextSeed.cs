7a70ba2b4576ae90058c365a9c4141c618247f44
Emiliano Bazán-Zapata <ebazan@midasconsultores.com.ar>
2022-08-15 11:40:23 -0300

    Implementar Interfaces en Infrastructure

---
+++ CleanArchitecture/CleanArchitecture.Data/Persistence/Seeds/StreeamerDbContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Infrastructure.Persistence.Seeds
+{
+    public class StreeamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context,
+                                           ILogger<StreeamerDbContextSeed> logger)
+        {
+            if (!context!.Streamers!.Any())
+            {
+                context!.Streamers!.AddRange(GetPreconfiguredStreamer());
+                await context!.SaveChangeAsync();
+                logger.LogInformation("Estamos insertando nuevos Records asl db {context}", typeof(StreamerDbContext).nam);
+            }
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamer()
+        {
+            return new List<Streamer>
+            {
+                new Streamer
+                {
+                    CreatedBy = "Emiliano",
+                    Nombre = "Bazan HBP",
+                    Url = "http://www.hbp.com"
+                },
+                new Streamer
+                {
+                    CreatedBy = "Emiliano",
+                    Nombre = "Amazon VIP",
+                    Url = "http://www.amazonvip.com"
+                }
+            };
+        }
+    }
+}
--- CleanArchitecture/CleanArchitecture.Data/Persistence/StreamerDbContext.cs
+++ CleanArchitecture/CleanArchitecture.Data/Persistence/StreamerDbContext.cs
@@ -1,18 +1,34 @@
 ﻿using CleanArchitecture.Domain;
+using CleanArchitecture.Domain.Common;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
-namespace CleanArchitecture.Data
+namespace CleanArchitecture.Infrastructure.Persistence
 {
     public class StreamerDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
         {
-            optionBuilder.UseSqlServer("Data Source=DESKTOP-UHFA848\\SQLEXPRESS;Initial Catalog=StreamerDB;Integrated Security=True")
-                         .LogTo(Console.WriteLine,
-                                        new[] { DbLoggerCategory.Database.Command.Name },
-                                        LogLevel.Information)
-                         .EnableSensitiveDataLogging();
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "System";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "System";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
